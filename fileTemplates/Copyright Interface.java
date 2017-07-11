aedd5eb7f97b83ac22b877154bb5a5f9954ed08a
qxj0551 <benjamin.bethke@partner.bmw.de>
2017-07-11 16:30:45 +0200

    AI-2.1 <QXJ0551@LMUC580457 Create baseRefactoring.xml, ide.general.xml, git.xml, TimberLog.xml, project.default.xml, diff.xml, Copyright Class.java, other.xml, A4A Style Schema.xml, androidStudioFirstRun.xml, file.template.settings.xml, find.xml, ui.lnf.xml, File Header.java, filetypes.xml, keymap.xml, androidEditors.xml, gradle.run.settings.xml, code.style.schemes.xml, AndroidLog.xml, notifications.xml, Copyright Interface.java, Default.xml, laf.xml, jdk.table.xml, debugger.renderers.xml, A4A Schema.xml, path.macros.xml, Default _1_.xml, web-browsers.xml, editor.codeinsight.xml, Darcula Input.icls, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/Copyright Interface.java
@@ -0,0 +1,18 @@
+/*
+
+ * Copyright (C) 2011-2015 BMW GROUP.
+
+ * All rights reserved.
+
+ *
+
+ * Project    : A4A Android: Connected library
+
+ * Department : EI-61
+
+ */
+
+
+public interface ${NAME} {
+
+}
---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,3 @@
+/**
+ * TODO: Add a class header comment!
+ */
